fbd7d63abfb446df6d6407a84f406fa27aa1cba9
Bram <bramstrik@live.com>
2025-03-21 10:55:50 +0100

    Make everything work

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import Card from './Card'
 
+function createRandomImageArray(length) {
+  function createImageObject() {
+    const getRandomImageSeed = () => crypto.randomUUID().split('-')[0];
+    return {key: crypto.randomUUID(), seed: getRandomImageSeed()};
+  }
+
+  const newArray = new Array(length).fill(null);
+  return newArray.map(createImageObject);
+}
+
 function App() {
-  // const [count, setCount] = useState(0)
+  const [images, setImages] = useState(createRandomImageArray(12));
+  const [clickedKeys, setClickedKeys] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  function shuffleImages() {
+    const shuffledImages = [...images];
+    shuffledImages.sort(() => Math.random() - 0.5);
+    setImages(shuffledImages);
+  }
+
+  function clickImage(key) {
+    return function () {
+      if (clickedKeys.includes(key)) {
+        setClickedKeys([]);
+        setScore(0);
+      } else {
+        if (score + 1 > bestScore) {
+          setBestScore(score + 1);
+        }
+        setScore(score + 1);
+        setClickedKeys([...clickedKeys, key]);
+      }
+      shuffleImages();
+    }
+  }
 
   return (
     <>
       <header>
         <h1>React Memory Game</h1>
         <div>
-          <p>current score: {'score'}</p>
-          <p>best score: {'bestScore'}</p>
+          <p>current score: {score}</p>
+          <p>best score: {bestScore}</p>
         </div>
       </header>
       <main>
@@ -22,22 +57,13 @@ function App() {
           <li>After each click, the cards are shuffled</li>
         </ol>
         <div className="cards-wrapper">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            images.map(image => <Card clickHandler={clickImage(image.key)} key={image.key} imageSeed={image.seed} />)
+          }
         </div>
       </main>
     </>
-  )
+  );
 }
 
 export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,35 @@
-import './Card.css'
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import './Card.css';
 
-function Card({imageLink = 'vite.svg'}) {
-  return <div className="card">
+function Card({imageSeed, clickHandler}) {
+  const [image, setImage] = useState(imageSeed)
+  useEffect(() => {
+    const requestURL = `https://picsum.photos/seed/${imageSeed}/300/300`;
+    console.log(requestURL)
+    try {
+      fetch(requestURL)
+        .then(response => response)
+        .then(x => {
+          setImage(x.url);
+        });
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [imageSeed])
+
+  return <div className="card" onClick={clickHandler}>
             <div className="card-image-container">
-              <div className="card-image" style={{backgroundImage: `url(${imageLink})`}}></div>
+              <div className="card-image" style={{backgroundImage: `url(${image})`}}></div>
             </div>
             <hr />
             <h3 className="card-title">Lorem Ipsum</h3>
           </div>
+}
+
+Card.propTypes = {
+  imageSeed: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired
 }
 
 export default Card
